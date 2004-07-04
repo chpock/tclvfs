773b8e6d5370a978fc0e05084365ce9376f68175
andreas_kupries <andreas_kupries>
2004-07-04 23:41:54 +0000

    * library/vfsUtils.tcl (crc, zip): Modified the Trf adapter 	  commands. They are now adding in a "--" before the last argument 	  they get, to ensure its interpretation as data instead of being 	  misinterpreted as option should it begin with a dash. Without 	  this adding a file beginning with a dash to a filesystem will 	  fail.

--- library/vfsUtils.tcl
+++ library/vfsUtils.tcl
@@ -1,6 +1,6 @@
 # vfsUtils.tcl --
 #
-# $Id: vfsUtils.tcl,v 1.26 2003/05/14 18:29:14 vincentdarley Exp $
+# $Id: vfsUtils.tcl,v 1.27 2004/07/04 23:42:04 andreas_kupries Exp $
 
 package require Tcl 8.4
 package require vfs
@@ -21,13 +21,13 @@ proc ::vfs::memchan {args} {
 # This can be overridden to use a different crc implementation
 proc ::vfs::crc {args} {
     ::package require Trf
-    uplevel 1 [list ::crc] $args
+    uplevel 1 [linsert [linsert $args end-1 "--"] 0 ::crc]
 }
 
 # This can be overridden to use a different zip implementation
 proc ::vfs::zip {args} {
     ::package require Trf
-    uplevel 1 [list ::zip] $args
+    uplevel 1 [linsert [linsert $args end-1 "--"] 0 ::zip]
 }
 
 proc ::vfs::autoMountExtension {ext cmd {pkg ""}} {
