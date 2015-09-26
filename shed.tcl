0c4d6ee1c0b0b57d4c2ba3c64ed9ebc26883689a
hypnotoad <hypnotoad>
2015-09-26 02:12:17 +0000

    Updates to the latest SHED standard

---
+++ shed.tcl
@@ -0,0 +1,20 @@
+###
+# This file is loaded by the tool object
+###
+
+###
+# Add in the C Code
+###
+my package add vfs {
+  class: binary
+  version: 1.4.2
+  sources: {generic/vfs.c}
+  init: Vfs_Init
+}
+
+###
+# Build the module section
+###
+foreach path [glob [file join $::TOOL_ROOT library *]] {
+  my module scan $path
+}
